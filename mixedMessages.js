dd6177b553c4e51f81416c80805c010cc28f43b8
Vinícius Queiróz de Oliveira <vqovinicius@gmail.com>
2021-09-28 15:45:17 -0300

    Set the categories of the messages components

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,1 +1,8 @@
 //console.log('Testing Node'); - Node is up and running
+//Creating a system that will store the messages components.
+const messageComponents = {
+    _firsPart: [],
+    _secondPart: [],
+    _thirPart: [],
+    _fourthPart: [],
+}
