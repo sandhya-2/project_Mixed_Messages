fbbff0582bbd805cf23f0471a872454c40a777cc
Vinícius Queiróz de Oliveira <vqovinicius@gmail.com>
2021-09-28 18:40:30 -0300

    Fulfill message components

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,8 +1,13 @@
 //console.log('Testing Node'); - Node is up and running
 //Creating a system that will store the messages components.
 const messageComponents = {
-    _firsPart: [],
-    _secondPart: [],
-    _thirPart: [],
-    _fourthPart: [],
-}
+    _beginsWithWhen: ['When life ', 'When you ', 'When your friends ', 'When your family ', 'When  everything ', 'When nothing '],
+    _beginsWithIf: ['If life ', 'If you ', 'If your heart ', 'If your guts ', 'If destiny ', 'If love ', 'If hate '],
+    _aboutLife: ['seems to be getting harder, ', 'gives you lemons, ', 'leads you on an unexpected path, '],
+    _aboutFeelings: ['feel like you are alone, ', 'are in love, ', 'get in love, ', 'have a crush, ', 'believe in yourself, ', 'gets in your way, '],
+    _aboutIntuition: ['tells you to do something, ', 'sets you on a different path, ', 'changes your plan, '],
+    _abaoutFamily: ['is far away, ', 'is gone, ', 'loves you, ', 'is the most important thing for you, ', 'falls apart, '],
+    _conclusionOfHope: ['keep your head up and keep moving forward!', 'stay strong and keep doing what you are doing!', 'keep working to make your dreams come true!'],
+    _conclusionOfTriumph: ['make a lemonade!', 'keep fighting until you overcome your dificulties!', 'look at what you have conquered so far and remember who you are!', 'remeber your strength and go trhough it!'],
+    _conclusionOfLove: ['show all the love you have!', 'feel the love inside your heart...', 'fulfill your thoughts wiht love and passion!'],
+};
